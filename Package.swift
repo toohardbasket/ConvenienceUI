6f2ac4725d00edf0fa6257e33f2212c8807f8bda
Evan Sunley James <evan@toohardbasket.com>
2025-08-09 19:46:16 +1200

    Update package support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ConvenienceUI",
     platforms: [
         .iOS(.v26),
-        .macOS(.v26)
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
